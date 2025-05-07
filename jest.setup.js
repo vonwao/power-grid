97b10bfb4047694c3338961df7c5552a341d4cbc
Otto Wachter <Otto.Wachter@albertsons.com>
2025-05-07 16:02:23 -0400

    x

--- jest.setup.js
+++ jest.setup.js
@@ -1,6 +1,10 @@
 // Import Jest DOM extensions
 require('@testing-library/jest-dom');
 
+// Set up Jest DOM matchers
+const matchers = require('@testing-library/jest-dom/matchers');
+expect.extend(matchers);
+
 // Mock the MUI X Data Grid
 jest.mock('@mui/x-data-grid', () => {
   return {
---
+++ types/jest-dom.d.ts
@@ -0,0 +1,29 @@
+import '@testing-library/jest-dom';
+
+declare global {
+  namespace jest {
+    interface Matchers<R> {
+      toBeInTheDocument(): R;
+      toBeDisabled(): R;
+      toBeEnabled(): R;
+      toBeEmpty(): R;
+      toBeEmptyDOMElement(): R;
+      toBeInvalid(): R;
+      toBeRequired(): R;
+      toBeValid(): R;
+      toBeVisible(): R;
+      toContainElement(element: HTMLElement | null): R;
+      toContainHTML(htmlText: string): R;
+      toHaveAttribute(attr: string, value?: string): R;
+      toHaveClass(...classNames: string[]): R;
+      toHaveFocus(): R;
+      toHaveFormValues(expectedValues: Record<string, any>): R;
+      toHaveStyle(css: string | Record<string, any>): R;
+      toHaveTextContent(text: string | RegExp, options?: { normalizeWhitespace: boolean }): R;
+      toHaveValue(value?: string | string[] | number): R;
+      toBeChecked(): R;
+      toBePartiallyChecked(): R;
+      toHaveDescription(text?: string | RegExp): R;
+    }
+  }
+}
